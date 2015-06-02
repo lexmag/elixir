ff1d0983cd408f8d5c46f2b0d028259ff5f14989
Aleksei Magusev <lexmag@me.com>
2015-06-03 01:22:56 +0200

    WIP (ExUnit.CaptureLog)

---
+++ lib/ex_unit/lib/ex_unit/capture_log.ex
@@ -0,0 +1,25 @@
+defmodule ExUnit.CaptureLog do
+  def capture_log(_opts \\ [], fun) do
+    old_gl = Process.group_leader()
+    {:ok, gl} = IO.Proxy.start_link()
+    handler = {Logger.Backends.Capture, make_ref()}
+    try do
+      Process.group_leader(self(), gl)
+      :ok = GenEvent.add_handler(Logger, handler, gl)
+      _ = fun.()
+      :ok
+    catch
+      kind, reason ->
+        stack = System.stacktrace()
+        _ = GenEvent.remove_handler(Logger, handler, nil)
+        :erlang.raise(kind, reason, stack)
+    else
+      :ok ->
+        {:ok, output} = GenEvent.remove_handler(Logger, handler, :get)
+        output
+    after
+      Process.group_leader(self(), old_gl)
+      IO.Proxy.stop(gl)
+    end
+  end
+end
