5b52ee5df0263821bf1d19bab5b0984c026ecbbf
Aleksei Magusev <lexmag@me.com>
2015-06-03 16:51:59 +0200

    WIP (IO.Proxy)

--- lib/elixir/lib/io/proxy.ex
+++ lib/elixir/lib/io/proxy.ex
@@ -5,9 +5,17 @@ defmodule IO.Proxy do
     GenServer.start_link(__MODULE__, nil)
   end
 
+  def stop(proxy, timeout \\ 5000) do
+    GenServer.call(proxy, :stop, timeout)
+  end
+
   def handle_info({:io_request, from, ref, req}, state) do
     reply = :io.request(Process.group_leader(), req)
     send(from, {:io_reply, ref, reply})
     {:noreply, state}
+  end
+
+  def handle_call(:stop, _from, state) do
+    {:stop, :normal, :ok, state}
   end
 end
