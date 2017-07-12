02e07689f131e30ebe90ea2109df6296e7a20f4b
Aleksei Magusev <lexmag@me.com>
2017-07-13 01:43:10 +0200

    Improve variable name in ExUnit.RunnerStats

--- lib/ex_unit/lib/ex_unit/runner_stats.ex
+++ lib/ex_unit/lib/ex_unit/runner_stats.ex
@@ -11,31 +11,31 @@ defmodule ExUnit.RunnerStats do
     GenServer.call(pid, :stats, :infinity)
   end
 
-  def handle_call(:stats, _from, map) do
-    {:reply, map, map}
+  def handle_call(:stats, _from, stats) do
+    {:reply, stats, stats}
   end
 
   def handle_cast({:test_finished, %ExUnit.Test{state: {tag, _}}},
-                  %{total: total, failures: failures} = map) when tag in [:failed, :invalid] do
-    {:noreply, %{map | total: total + 1, failures: failures + 1}}
+                  %{total: total, failures: failures} = stats) when tag in [:failed, :invalid] do
+    {:noreply, %{stats | total: total + 1, failures: failures + 1}}
   end
 
   def handle_cast({:test_finished, %ExUnit.Test{state: {:skip, _}}},
-                  %{total: total, skipped: skipped} = map) do
-    {:noreply, %{map | total: total + 1, skipped: skipped + 1}}
+                  %{total: total, skipped: skipped} = stats) do
+    {:noreply, %{stats | total: total + 1, skipped: skipped + 1}}
   end
 
   def handle_cast({:case_finished, %ExUnit.TestCase{state: {:failed, _failures}} = test_case},
-                  %{failures: failures, total: total} = map) do
+                  %{failures: failures, total: total} = stats) do
     test_count = length(test_case.tests)
-    {:noreply, %{map | failures: failures + test_count, total: total + test_count}}
+    {:noreply, %{stats | failures: failures + test_count, total: total + test_count}}
   end
 
-  def handle_cast({:test_finished, _}, %{total: total} = map) do
-    {:noreply, %{map | total: total + 1}}
+  def handle_cast({:test_finished, _}, %{total: total} = stats) do
+    {:noreply, %{stats | total: total + 1}}
   end
 
-  def handle_cast(_, map) do
-    {:noreply, map}
+  def handle_cast(_, stats) do
+    {:noreply, stats}
   end
 end
